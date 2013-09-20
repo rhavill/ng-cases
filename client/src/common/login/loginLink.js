36150b132a71408a706a2fec9ef133816dc37e05
Ricardo Havill <rhavill@progresstesting.com>
2013-09-19 22:30:18 -0400

    Created template file for loginLink directive.

--- client/src/common/login/loginLink.js
+++ client/src/common/login/loginLink.js
@@ -2,7 +2,7 @@ angular.module( 'login.link', [] )
 
     .directive( 'loginLink', function() {
         return {
-            template: '<a href="#/login"><i class="icon-user"></i>&nbsp;Login</a>'
+            templateUrl: 'login/loginLink.tpl.html'
         };
     })
 
