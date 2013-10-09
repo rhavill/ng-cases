9fda9a44a4b401d670ff79c82e31d55593ff3eac
Ricardo Havill <rhavill@progresstesting.com>
2013-10-08 21:51:06 -0400

    Changed login directive to return object variable.

--- client/src/common/login/login.js
+++ client/src/common/login/login.js
@@ -2,9 +2,14 @@ angular.module('login', [
     'login.link',
     'login.form'])
 .factory('login', ['$http', function($http) {
-    return {
+    var service =  {
+        user: null,
         isAuthenticated: function() {
+            if (service.user) {
+                return true;
+            }
             return false;
         }
     };
+    return service;
 }]);
