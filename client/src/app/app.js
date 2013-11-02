9244c887e7a4983be448527953bb5711da8e9543
Ricardo Havill <rhavill@progresstesting.com>
2013-11-01 22:29:57 -0400

    Remove "ngBoilerplate" from page title.

--- client/src/app/app.js
+++ client/src/app/app.js
@@ -19,7 +19,7 @@ angular.module('ngBoilerplate', [
     .controller('AppCtrl', function AppCtrl($scope, $location, $state) {
       $scope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
         if (angular.isDefined(toState.data.pageTitle)) {
-          $scope.pageTitle = toState.data.pageTitle + ' | ngBoilerplate';
+          $scope.pageTitle = toState.data.pageTitle;
         }
         if (toState.name == 'private' || !login.isAuthenticated()) {
           $state.go('login');
