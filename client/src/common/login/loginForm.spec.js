cef95653d75887f4cb77f1e277b518bbd869f824
Ricardo Havill <rhavill@progresstesting.com>
2013-11-18 00:41:13 -0500

    Changed tests for login form controller to use mock login service instead of login service.

--- client/src/common/login/loginForm.spec.js
+++ client/src/common/login/loginForm.spec.js
@@ -18,47 +18,64 @@ describe('login form', function () {
     });
 
     describe('LoginFormController', function(){
-      var scope, ctrl, $httpBackend;
+      var $scope, ctrl;
+      var mockLoginService = {
+        user: null,
+        isAuthenticated: function (){
+          if (mockLoginService.user) {
+            return true;
+          }
+          else {
+            return false;
+          }
+        },
+        login: function(username,password) {
+          if (password == 'correct') {
+            mockLoginService.user = {id: 1, username: 'test'};
+          }
+          else {
+            mockLoginService.user = null;
+          }
+          if (username == 'server_error') {
+            return { then: function(success, failure) {failure();}};
+          }
+          return { then: function(success, failure) {success();}};
+        }
+      };
+      var mockStateService = {
+        go: function (state){
+        }
+      };
 
-      beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
-        $httpBackend = _$httpBackend_;
-        scope = $rootScope.$new();
-        ctrl = $controller('LoginFormController', {$scope: scope});
+      beforeEach(inject(function($rootScope, $controller) {
+        $scope = $rootScope.$new();
+        ctrl = $controller('LoginFormController', {$scope: $scope, login: mockLoginService});
       }));
 
       it('should set some default values.', function() {
-        expect(scope.authError).toBe(null);
-        expect(scope.user).toBe(null);
+        expect($scope.authError).toBe(null);
+        expect($scope.user).toBe(null);
       });
 
       it('should unset user and set authentication error after failed login.', function() {
-        $httpBackend.expectPOST('/login', {username: 'test', password: 'wrong'}).
-            respond({id:0});
-        scope.user = {username:'test',password:'wrong'};
-        scope.login();
-        $httpBackend.flush();
-        expect(scope.authError).toBe('Bad username or password.');
-        expect(scope.user).toBe(null);
+        $scope.user = {username:'test',password:'wrong'};
+        $scope.login();
+        expect($scope.authError).toBe('Bad username or password.');
+        expect($scope.user).toBe(null);
       });
 
       it('should set user and not set authentication error after successful login.', function() {
-        $httpBackend.expectPOST('/login', {username: 'test', password: 'right'}).
-            respond({id:1, username: 'test'});
-        scope.user = {username:'test',password:'right'};
-        scope.login();
-        $httpBackend.flush();
-        expect(scope.authError).toBe(null);
-        expect(scope.user).toEqualData({id:1, username: 'test'});
+        $scope.user = {username:'test',password:'correct'};
+        $scope.login();
+        expect($scope.authError).toBe(null);
+        expect($scope.user).toEqualData({id:1, username: 'test'});
       });
 
       it('should unset user and set authentication error after failure to contact server.', function() {
-        $httpBackend.expectPOST('/login', {username: 'test', password: 'right'}).
-            respond(500,'');
-        scope.user = {username:'test',password:'right'};
-        scope.login();
-        $httpBackend.flush();
-        expect(scope.authError).toBe('Error logging in. Problem contacting server.');
-        expect(scope.user).toBe(null);
+        $scope.user = {username:'server_error',password:'correct'};
+        $scope.login();
+        expect($scope.authError).toBe('Error logging in. Problem contacting server.');
+        expect($scope.user).toBe(null);
       });
 
     });
